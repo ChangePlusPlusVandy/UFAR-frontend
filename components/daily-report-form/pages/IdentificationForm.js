0d2ed26916f9de18b0e032276d9fd1fefbb5f49a
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2021-12-06 00:20:17 -0600

    Implement dropdown menus with dummy items

--- components/daily-report-form/pages/IdentificationForm.js
+++ components/daily-report-form/pages/IdentificationForm.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import {StyleSheet, Text, TextInput, View} from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
+import {Chevron} from 'react-native-shapes';
+
+// Dropdown items
+const provinceNames = [
+    { label: 'Kwango', value: 'kwango' },
+    { label: 'Kwilu', value: 'kwilu' },
+];
+const healthZoneNames = [
+    { label: 'Health Zone 1', value: 'healthZone1' },
+    { label: 'Health Zone 2', value: 'healthZone2' },
+    { label: 'Health Zone 3', value: 'healthZone3' },
+];
+const healthAreas = [
+    { label: 'Health Area 1', value: 'healthArea1' },
+    { label: 'Health Area 2', value: 'healthArea2' },
+    { label: 'Health Area 3', value: 'healthArea3' },
+];
+const villageNames = [
+    { label: 'Village 1', value: 'village1' },
+    { label: 'Village 2', value: 'village2' },
+    { label: 'Village 3', value: 'village3' },
+];
 
 export default function IdentificationForm(props) {
     return (
@@ -13,17 +35,40 @@ export default function IdentificationForm(props) {
                 </View>
                 <RNPickerSelect
                     useNativeAndroidPickerStyle={false}
-                    style={{inputAndroid: {...styles.inputField}}}
+                    style={{inputAndroid: styles.inputField, iconContainer: styles.RNPickerSelectIconContainer, placeholder: styles.placeholder}}
                     onValueChange={(value) => props.setProvinceName(value)}
-                    items={[
-                        { label: 'Kwango', value: 'kwango' },
-                        { label: 'Kwilu', value: 'kwilu' },
-                    ]}
-                    defaultValue={props.provinceName}
-                    placeholder={{label: "Name of Province/Region", value: null}}/>
-                <TextInput onChange={(e) => props.setHealthZoneName(e.nativeEvent.text)}  style={styles.inputField} defaultValue={props.healthZoneName} placeholder="Health Zone Name" />
-                <TextInput onChange={(e) => props.setHealthArea(e.nativeEvent.text)} style={styles.inputField} defaultValue={props.healthArea} placeholder="Aire de santé" />
-                <TextInput onChange={(e) => props.setVillageName(e.nativeEvent.text)} style={styles.inputField} defaultValue={props.villageName} placeholder="Village/Community Name" />
+                    items={provinceNames}
+                    value={props.provinceName}
+                    placeholder={{label: 'Name of Province/Region', value: null}}
+                    Icon={() => <Chevron size={1.5} color='#9D9D9D' />}
+                />
+                <RNPickerSelect
+                    useNativeAndroidPickerStyle={false}
+                    style={{inputAndroid: styles.inputField, iconContainer: styles.RNPickerSelectIconContainer, placeholder: styles.placeholder}}
+                    onValueChange={(value) => props.setHealthZoneName(value)}
+                    items={healthZoneNames}
+                    value={props.healthZoneName}
+                    placeholder={{label: 'Health Zone Name', value: null}}
+                    Icon={() => <Chevron size={1.5} color='#9D9D9D' />}
+                />
+                <RNPickerSelect
+                    useNativeAndroidPickerStyle={false}
+                    style={{inputAndroid: styles.inputField, iconContainer: styles.RNPickerSelectIconContainer, placeholder: styles.placeholder}}
+                    onValueChange={(value) => props.setHealthArea(value)}
+                    items={healthAreas}
+                    value={props.healthArea}
+                    placeholder={{label: 'Aire de santé', value: null}}
+                    Icon={() => <Chevron size={1.5} color='#9D9D9D' />}
+                />
+                <RNPickerSelect
+                    useNativeAndroidPickerStyle={false}
+                    style={{inputAndroid: styles.inputField, iconContainer: styles.RNPickerSelectIconContainer, placeholder: styles.placeholder}}
+                    onValueChange={(value) => props.setVillageName(value)}
+                    items={villageNames}
+                    value={props.villageName}
+                    placeholder={{label: 'Village/Community Name', value: null}}
+                    Icon={() => <Chevron size={1.5} color='#9D9D9D' />}
+                />
                 {/* <TextInput style={styles.inputField} placeholder="GPS Location (autofill)" /> */}
             </View>
         </View>
@@ -53,5 +98,18 @@ const styles = StyleSheet.create({
         fontSize: 11,
         lineHeight: 13,
         color: 'black',
-    }
+    },
+    RNPickerSelectIconContainer: {
+        justifyContent: 'center',
+        top: 5,
+        right: 15,
+        height: 37,
+        borderLeftColor: '#D6D6D6',
+        borderLeftWidth: 0.5,
+        paddingTop: 4,
+        paddingLeft: 10,
+    },
+    placeholder: {
+        color: '#9D9D9D',
+    },
 })
