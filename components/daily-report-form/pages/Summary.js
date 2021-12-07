2e70e37119adab59adc2ac9e1f0f2096745a216c
Varun Sangal <varun.s.sangal@vanderbilt.edu>
2021-12-07 00:16:29 -0600

    Summary updates

--- components/daily-report-form/pages/Summary.js
+++ components/daily-report-form/pages/Summary.js
@@ -5,19 +5,47 @@ export default function Summary() {
     return (
         <View>
             <Text style={styles.header}>Summary</Text>
-            {/* TODO: summary report */}
+            <Text style={styles.line}>_________________</Text>
+            <View>
+                <Text style={styles.bod}>DMM Day: 1</Text>
+                <Text style={styles.bod}>Name: Varun Sangal</Text>
+                <Text style={styles.bod}>Province/Region: Kwango</Text>
+                <Text style={styles.bod}>Health Zone: Koshibanda</Text>
+                <Text style={styles.bod}>Village: Munga</Text>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     header: {
-        paddingVertical: 18,
+        paddingVertical: 5,
         alignSelf: 'center',
         fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Helvetica Neue',
         fontWeight: 'bold',
-        fontSize: 23,
-        lineHeight: 28,
+        fontSize: 40,
+        lineHeight: 50,
         color: 'white',
+    },
+    line: {
+        paddingVertical: 5,
+        alignSelf: 'flex-start',
+        fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Sans-Serif',
+        fontWeight: 'bold',
+        fontSize: 30,
+        lineHeight: 10,
+        color: 'white',
+        padding: "10%"
+    },
+    bod: {
+        paddingVertical: 20,
+        alignSelf: 'flex-start',
+        fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Sans-Serif',
+        fontWeight: 'normal',
+        fontSize: 25,
+        padding: "5%",
+        lineHeight: 10,
+        color: "white"
     }
+
 })
