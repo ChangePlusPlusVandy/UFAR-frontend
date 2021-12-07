333dfea359720e4bd64be8a3854e2a0f4cf7312d
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2021-12-06 18:19:26 -0600

    Implement input fields for Denumber page #1

--- components/daily-report-form/pages/Denumber1Form.js
+++ components/daily-report-form/pages/Denumber1Form.js
@@ -1,11 +1,46 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function Denumber1Form() {
     return (
         <View>
             <Text style={styles.header}>Denumber</Text>
-            {/* TODO: input fields */}
+            <ScrollView style={styles.inputContainer}>
+                <View style={styles.rowContainer}>
+                    <View style={styles.leftContainer}>
+                        <Text style={{...styles.inputLabel, fontSize: 17, lineHeight: 20}} >Men</Text>
+                        <Text style={styles.inputLabel}># of children {'<'} 6 months</Text>
+                        <TextInput style={styles.inputField} />
+                        <Text style={styles.inputLabel}># of children 6 months - {'<'}5 years</Text>
+                        <TextInput style={styles.inputField} />
+                        <Text style={styles.inputLabel}># of children 5 - {'<'}15 years</Text>
+                        <TextInput style={styles.inputField} />
+                        <Text style={styles.inputLabel}># of Persons 15 years and older</Text>
+                        <TextInput style={styles.inputField} />
+                    </View>
+                    <View style={styles.rightContainer}>
+                        <Text style={styles.inputLabel}>Total Number of Men</Text>
+                        <TextInput style={styles.inputField} />
+                    </View>
+                </View>
+                <View style={{...styles.rowContainer, marginTop: 20}}>
+                    <View style={styles.leftContainer}>
+                        <Text style={{...styles.inputLabel, fontSize: 17, lineHeight: 20}} >Women</Text>
+                        <Text style={styles.inputLabel}># of children {'<'} 6 months</Text>
+                        <TextInput style={styles.inputField} />
+                        <Text style={styles.inputLabel}># of children 6 months - {'<'}5 years</Text>
+                        <TextInput style={styles.inputField} />
+                        <Text style={styles.inputLabel}># of children 5 - {'<'}15 years</Text>
+                        <TextInput style={styles.inputField} />
+                        <Text style={styles.inputLabel}># of Persons 15 years and older</Text>
+                        <TextInput style={styles.inputField} />
+                    </View>
+                    <View style={styles.rightContainer}>
+                        <Text style={styles.inputLabel}>Total Number of Women</Text>
+                        <TextInput style={styles.inputField} />
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     );
 }
@@ -22,5 +57,36 @@ const styles = StyleSheet.create({
     },
     inputContainer: {
         marginHorizontal: 34,
+        marginBottom: 175,
     },
+    inputLabel: {
+        fontFamily: Platform.OS === 'android' ? 'sans-serif' : 'Avenir-Roman',
+        fontSize: 11,
+        lineHeight: 13,
+        color: 'white',
+    },
+    inputField: {
+        marginVertical: 5,
+        paddingVertical: 5,
+        paddingHorizontal: 15,
+        borderRadius: 17,
+        backgroundColor: 'white',
+        fontFamily: Platform.OS === 'android' ? 'sans-serif-medium' : 'Avenir-Roman',
+        fontSize: 11,
+        lineHeight: 13,
+        color: 'black',
+        width: 95,
+        textAlign: 'center',
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    leftContainer: {
+        flex: 1,
+    },
+    rightContainer: {
+        flex: 1,
+        alignItems: 'center',
+    }
 })
