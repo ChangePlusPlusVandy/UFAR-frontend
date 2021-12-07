dd95439720cc3c73481bc50604fdfc65c8944a23
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2021-12-06 18:33:46 -0600

    Implement input fields for Denumber page #2, adjusted font sizes

--- components/daily-report-form/pages/Denumber3Form.js
+++ components/daily-report-form/pages/Denumber3Form.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function Denumber3Form() {
     return (
         <View>
             <Text style={styles.header}>Denumber</Text>
-            {/* TODO: input fields */}
+            <View style={styles.inputContainer}>
+                <Text style={{...styles.inputLabel, fontSize: 19, lineHeight: 22}} >Households</Text>
+                <Text style={styles.inputLabel}>Number of households visited</Text>
+                <TextInput style={styles.inputField} />
+                <Text style={styles.inputLabel}>Total number of households treated</Text>
+                <TextInput style={styles.inputField} />
+                <Text style={styles.inputLabel}>Greographical coverage of the households</Text>
+                <TextInput style={styles.inputField} />
+            </View>
         </View>
     );
 }
@@ -22,5 +30,24 @@ const styles = StyleSheet.create({
     },
     inputContainer: {
         marginHorizontal: 34,
+    },
+    inputLabel: {
+        fontFamily: Platform.OS === 'android' ? 'sans-serif' : 'Avenir-Roman',
+        fontSize: 11,
+        lineHeight: 13,
+        color: 'white',
+    },
+    inputField: {
+        marginVertical: 5,
+        paddingVertical: 5,
+        paddingHorizontal: 15,
+        borderRadius: 17,
+        backgroundColor: 'white',
+        fontFamily: Platform.OS === 'android' ? 'sans-serif-medium' : 'Avenir-Roman',
+        fontSize: 11,
+        lineHeight: 13,
+        color: 'black',
+        width: 95,
+        textAlign: 'center',
     },
 })
