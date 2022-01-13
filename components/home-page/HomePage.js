e2429a340b919f1d24f028992cc012f37f0e16c6
Ethan Ratnofsky <43038541+ethanratnofsky@users.noreply.github.com>
2022-01-12 19:04:50 -0600

    Refactored home page styling

--- components/home-page/HomePage.js
+++ components/home-page/HomePage.js
@@ -1,88 +1,40 @@
 import React from 'react';
 import {StyleSheet , TouchableOpacity, View, Text, Image} from 'react-native';
-import {Icon} from 'react-native-elements';
+
+import AdminButton from './AdminButton';
+import NormalButton from './NormalButton';
 import ufar from './ufar.png';
 
 const IMAGE_DEF = Image.resolveAssetSource(ufar).uri;
 
-export default function HomePage () {
-    const onPress = () => {
-        // Do something
-        return;
-    }
+export default function HomePage() {
+  return (
+    <View style={styles.container}>
+      <Image source={{uri: IMAGE_DEF}} style={styles.image} />
+      <View style={styles.buttonsContainer}>
+        <AdminButton />
+        <NormalButton />
+      </View>
+    </View>
+  )
+}
 
-    return (
-        <View style={styles.screen}>
-            <Image
-            source={{uri: IMAGE_DEF}}
-            style={styles.image}
-          />
-          { <>
-          <TouchableOpacity
-          style={styles.roundButton1}>
-        </TouchableOpacity> 
-        
-        <TouchableOpacity
-          style={styles.roundButton2}>
-
-          </TouchableOpacity></> }
-          <Text style={styles.right}>Normal</Text>
-          <Text style={styles.left}>Admin</Text>
-        </View>
-      );
-    }
-    
-    /// Just some styles
-    const styles = StyleSheet.create({
-      screen: {
-        flex: 1,
-        backgroundColor: "#EC1C24",
-        height: "100%",
-        width: "100%",
-        alignSelf:'center',
-      },
-      image: {
-        justifyContent:'center',
-        position: 'absolute',
-        top: 81,
-        height: 79,
-        width: 360,
-        alignSelf:'center',
-      },
-      roundButton1: {
-        width: 54,
-        height: 54,
-        position: 'absolute',
-        left: 212,
-        top: 311,
-        borderRadius: 100,
-        backgroundColor: '#59AECF',
-        elevation: 10,
-      },
-      roundButton2: {
-        width: 54,
-        height: 54,
-        position: 'absolute',
-        left: 94,
-        top: 311,
-        borderRadius: 100,
-        backgroundColor: '#8AC566',
-      },
-      left: {
-        fontSize: 13,
-        position: 'absolute',
-        left: 102,
-        top: 375,
-        lineHeight: 16,
-        color: '#FFF',
-      },
-      right: {
-        fontSize: 13,
-        position: 'absolute',
-        left: 218,
-        top: 375,
-        lineHeight: 16,
-        color: '#FFF',
-      },
-
-    });
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#EC1C24",
+    height: "100%",
+    width: "100%",
+    justifyContent: 'center'
+  },
+  image: {
+    height: 79,
+    width: 360,
+    bottom: 200,
+    alignSelf:'center',
+  },
+  buttonsContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  }
+})
---
+++ components/home-page/NormalButton.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {TouchableOpacity, StyleSheet, Text, View} from 'react-native';
+
+export default function NormalButton() {
+    // Change function here
+    const incremenetActivePage = () => setActivePage((prev) => prev + 1);
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={incremenetActivePage} style={styles.button}>
+                {/* INSERT ICON */}
+            </TouchableOpacity>
+            <Text style={styles.text}>Normal</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+    },
+    button: {
+        backgroundColor: '#59AECF',
+        height: 54,
+        width: 54,
+        borderRadius: 27,
+        alignContent: 'center',
+        justifyContent: 'center',
+
+        /* Android Drop Shadow Styling */
+        elevation: 10,
+        
+        /* iOS Drop Shadow Styling */
+        shadowColor: "black",
+        shadowOffset: {
+            width: 10,
+            height: 10,
+        },
+        shadowRadius: 10,
+        shadowOpacity: 0.3,
+    },
+    text: {
+        paddingVertical: 3,
+        color: 'white',
+        fontFamily: Platform.OS === 'android' ? 'sans-serif-medium' : 'Avenir-Roman',
+        fontSize: 13,
+        lineHeight: 15,
+    }
+});
